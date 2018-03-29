2c1c82e5c31be8b08d20f7704560c97d50a32112
gladki24 <seweryngla@hotmail.com>
2018-03-29 23:32:48 +0200

    28.03.18 - refactoring 1#

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var cors = require("cors");
 var morgan = require("morgan");
+var bodyParser = require("body-parser");
 var database_js_1 = require("./database.js");
 var Product = require("./routes/product");
 var Random = require("./routes/random");
 var SmallTile = require("./routes/smallTile");
 var Detail = require("./routes/detail");
+var User = require("./routes/user");
 var app = express();
 // Config
 app.use(cors());
 app.use(express.static('./public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/product', Product.router);
 app.use('/random', Random.router);
 app.use('/tile', SmallTile.router);
 app.use('/detail', Detail.router);
+app.use('/user', User.router);
 app.use(morgan(':remote-addr :method :url :status :res[content-length] - :response-time ms'));
 // App start
 app.listen(3000, function () {
