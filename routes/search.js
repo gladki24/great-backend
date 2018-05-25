b56ea36d7d23c0d91c208407f3204164448792d0
gladki24 <seweryngla@hotmail.com>
2018-05-25 10:07:55 +0200

    25.05.2018 - add search.js and tag.js

---
+++ routes/search.js
@@ -0,0 +1,56 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const morgan = require("morgan");
+const database_1 = require("../database");
+const Enums_1 = require("../Util/Enums");
+const router = express.Router();
+exports.router = router;
+router.use(morgan(':remote-addr :method :url :status :res[content-length] - :response-time ms'));
+router.get('/:number/:type/:query', (req, res) => {
+    let sql;
+    switch (parseInt(req.params.type, 0)) {
+        case Enums_1.ESearchType.Name: {
+            sql = `
+            SELECT product.id AS id,
+            product.title AS title,
+            brand.name as brandName,
+            product.image_source as imgSource,
+            brand.logo_source as logoSource
+            FROM product
+            JOIN brand
+            ON product.brand_id = brand.id
+            WHERE product.title LIKE '%${req.params.query}%'
+            LIMIT ${req.params.number}
+            `;
+            break;
+        }
+        case Enums_1.ESearchType.Tag: {
+            sql = `
+            SELECT product.id AS id,
+            product.title AS title,
+            brand.name as brandName,
+            product.image_source as imgSource,
+            brand.logo_source as logoSource
+            FROM product
+            JOIN brand
+            ON product.brand_id = brand.id
+            JOIN product_tag_user
+            ON product.id = product_tag_user.product_id
+            WHERE product_tag_user.tag_id LIKE '%${req.params.query}%'
+            LIMIT ${req.params.number}
+            `;
+            break;
+        }
+    }
+    database_1.database.query(sql, (err, rows, fields) => {
+        if (err) {
+            console.log(err);
+            res.status(404).json(false);
+        }
+        else {
+            res.status(200).json(rows);
+        }
+    });
+});
+//# sourceMappingURL=search.js.map
---
+++ routes/tag.js
@@ -0,0 +1,86 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const morgan = require("morgan");
+const database_1 = require("../database");
+const router = express.Router();
+exports.router = router;
+router.use(morgan(':remote-addr :method :url :status :res[content-length] - :response-time ms'));
+router.get('/get/:id', (req, res) => {
+    const sql = `
+   SELECT tag.id as id,
+   COUNT(product_tag_user.user_id) as number
+   FROM tag
+   JOIN product_tag_user
+   ON tag.id = product_tag_user.tag_id
+   WHERE product_tag_user.product_id = '${req.params.id}'
+   GROUP BY tag_id
+   ORDER BY COUNT(product_tag_user.user_id) DESC
+   `;
+    database_1.database.query(sql, (err, rows, fields) => {
+        if (err) {
+            console.log(err);
+            res.status(409).json(false);
+        }
+        else {
+            res.status(200).json(rows);
+        }
+    });
+});
+router.post('/add', (req, res) => {
+    const sql = `
+    SELECT *
+    FROM tag
+    WHERE id = '${req.body.tag}'
+    `;
+    database_1.database.query(sql, (err, rows, fields) => {
+        let sql2;
+        if (rows) {
+            sql2 = `
+            INSERT INTO product_tag_user
+            (product_id, tag_id, user_id)
+            VALUES
+            ('${req.body.product}','${req.body.tag}','${req.body.user}')
+            `;
+        }
+        else {
+            sql2 = `
+            INSERT INTO tag
+            (id)
+            VALUES
+            ('${req.body.tag}');
+            INSERT INTO product_tag_user
+            (product_id, tag_id, user_id)
+            VALUES
+            ('${req.body.product}','${req.body.tag}','${req.body.user}')
+            `;
+        }
+        database_1.database.query(sql2, (err2, rows2, fields2) => {
+            if (err) {
+                console.log(err2);
+                res.status(409).json(false);
+            }
+            else {
+                res.status(200).json(true);
+            }
+        });
+    });
+});
+router.post('/vote', (req, res) => {
+    const sql = `
+    INSERT INTO product_tag_user
+    (product_id, tag_id, user_id)
+    VALUES
+    ('${req.body.product}','${req.body.tag}','${req.body.user}')
+    `;
+    database_1.database.query(sql, (err, rows, fields) => {
+        if (err) {
+            console.log(err);
+            res.status(409).json(false);
+        }
+        else {
+            res.status(200).json(true);
+        }
+    });
+});
+//# sourceMappingURL=tag.js.map
